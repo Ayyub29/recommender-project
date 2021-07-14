62897e5b946510f561d34600c96e901299685268
ayyub29 <ayyub.shylsp@gmail.com>
2021-07-14 14:05:06 +0700

    halodoc

--- client/src/routes/Login.svelte
+++ client/src/routes/Login.svelte
@@ -31,7 +31,7 @@
         Api.post("/api/auth/login", loginField)
           .then((result) => {
             localStorage.setItem('user', JSON.stringify(result.data));
-            Api.post("/api/user/belikue", kue);
+            Api.post("/api/user/belikue", kue).then((res) => console.log(res));
             window.location.href = '/';
             isSuccess = true;
             isLoading = false;
