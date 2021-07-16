15a7c0e9c9e26a21cbb4fbe6fa966c5d45d36887
ayyub29 <ayyub.shylsp@gmail.com>
2021-07-16 16:54:21 +0700

    alo

--- controller/similarity.js
+++ controller/similarity.js
@@ -24,21 +24,19 @@ exports.beliKue = async (req, res) => {
     }
 }
 
-exports.liatKue = async (req, res) => {
-    try {
-        const kue = req.query.kue;
-        const userQueryResp = await new Promise((resolve, reject) => {
-            con.query(`SELECT * FROM cardsimilarity`, (err, result)=>{
-                if(err) throw err;
-                res.status(200)
-                .send({ message: "kuenya laku 1",
-                data: result });
-            })    
-        });
-    } catch(e) {
-      console.log(e);
-      res
-      .status(500)
-      .send({msg: e.stack});
-    }
+exports.liatkue = async (req, res) => {
+	try {
+		con.query(`SELECT * FROM cardsimilarity`, (err, result)=>{
+			if (err) throw err;
+			console.log(result);
+			res.status(200).send({
+				message: "Here is your Request",
+				data: result
+			})
+		})    
+	} catch (e) {
+		console.log(e.stack);
+		res.status(400)
+   		 .send({msg: e.stack});
+	}
 }
